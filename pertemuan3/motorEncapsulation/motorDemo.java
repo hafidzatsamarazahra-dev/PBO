7b280470d5292d224b3dd3df5d6db71d1683fd30
hafidza <hafidzatsamarazahra@gmail.com>
2026-09-11 08:57:48 +0700

    percobaan 2

--- pertemuan3/motorEncapsulation/motorDemo.java
+++ pertemuan3/motorEncapsulation/motorDemo.java
@@ -4,8 +4,22 @@ public class motorDemo {
     public static void main(String[] args) {
         Motor motor = new Motor();
         motor.printStatus();
-        motor.kecepatan = 50;
-        motor.printStatus();
+       motor.tambahKecepatan();
+
+       motor.nyalakanMesin();
+       motor.printStatus();
+
+       motor.tambahKecepatan();
+       motor.printStatus();
+
+       motor.tambahKecepatan();
+       motor.printStatus();
+
+       motor.tambahKecepatan();
+       motor.printStatus();
+
+       motor.matikanMesin();
+       motor.printStatus();
     }
     
 }
