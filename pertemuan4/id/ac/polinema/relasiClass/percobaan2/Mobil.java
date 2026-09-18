442a69a55928ef71efb5c3aebeb8bf2eddaf4c2b
hafidza <hafidzatsamarazahra@gmail.com>
2026-09-18 10:23:59 +0700

    percobaan 2

---
+++ pertemuan4/id/ac/polinema/relasiClass/percobaan2/Mobil.java
@@ -0,0 +1,30 @@
+package pertemuan4.id.ac.polinema.relasiClass.percobaan2;
+
+public class Mobil {
+    private String merk;
+    private int biaya;
+
+    public Mobil() {
+    }
+
+    public void setMerk(String merk) {
+    this.merk = merk;
+    }
+
+    public String getMerk() {
+    return merk;
+    }
+
+    public void setBiaya(int biaya) {
+    this.biaya = biaya;
+    }
+
+    public int getBiaya() {
+    return biaya;
+    }
+
+    public int hitungBiayaMobil(int hari) {
+    return biaya * hari;
+    }
+
+}
